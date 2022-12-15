f6f288747c71ad017718f31465912d1d56bf2b64
pontus393 <pontus.loefgren@gmail.com>
2022-12-15 17:29:41 +0100

    Pseudocode getComputerChoise()

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-console.log("Hello World!")
+// Create intVar containing a random value between 0 - 2
+// Create if statements evaluating value and returning "Rock", "Paper" or "Scissors" depending on the value
+// Create stringVar containing the result
