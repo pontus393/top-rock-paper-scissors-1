8bef07ade25b1e6355ace143d24288037006a620
pontus393 <pontus.loefgren@gmail.com>
2022-12-15 18:14:02 +0100

    Create function playRound and test that is retreives computerChoice

--- script.js
+++ script.js
@@ -22,4 +22,18 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice())
+// FUNCTION FOR GAME ROUND WITH PLAYER AND COMPUTER CHOICE
+function playRound(player, comp) {
+
+    let playerChoice = player;
+    let computerChoice = comp;
+
+    return player + " " + comp;
+}
+
+console.log(playRound("Paper", getComputerChoice()));
+
+// Create stringVar containing player choice (to be updated with input at later stage)
+
+// Create switch statements for all possible combinations
+
